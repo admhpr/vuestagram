bc6077d47aae585ac5467640378ec9a7631ef86c
Adam Harpur <adam@harpur.io>
2018-12-28 13:12:17 -0600

    file upload

--- src/components/PhoneBody.vue
+++ src/components/PhoneBody.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="phone-body">
-    <div class="feed">
+    <div v-if="step === 1" class="feed">
       <post v-for="post in posts" :post="post" :key="posts.indexOf(post)"></post>
+    </div>
+    <div v-if="step === 2">
+      <div class="selected-image" :style="{ backgroundImage: 'url(' + image + ')' }"></div>
+      <div class="filter-container">Step two
+        <!-- Where filter choices will be -->
+      </div>
     </div>
   </div>
 </template>
@@ -12,8 +18,10 @@ import Post from "./Post";
 export default {
   name: "PhoneBody",
   props: {
+    step: Number,
     posts: Array,
-    filters: Array
+    filters: Array,
+    image: String
   },
   components: {
     post: Post
